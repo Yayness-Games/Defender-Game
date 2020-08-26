74a4cad2a070946953bdcb6f3b57244ce0ce2af4
TodTheSushiMonster <69654470+TodTheSushiMonster@users.noreply.github.com>
2020-08-26 16:47:05 -0700

    Exploding rocks
    If you hit a rocks it explodes into a bunch of little rocks and you can shoot those rocks and they die. A bunch of stuff you can mess around with.

---
+++ Assets/Scripts/SmallRock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallRock : MonoBehaviour
+{
+
+    [SerializeField] float health = 1;
+    [SerializeField] int scoreValue = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("BOOM");
+
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        }
+    }
+
+}
