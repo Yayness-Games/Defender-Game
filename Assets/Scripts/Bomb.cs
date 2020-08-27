4810776d116664ac7d9eeec0110aca7d986cd4d3
TodTheSushiMonster <69654470+TodTheSushiMonster@users.noreply.github.com>
2020-08-27 10:47:07 -0700

    Rocks that actually explode
    The smaller rocks explode now so yay. Also there is a bomb class you can easily call once instantiated

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<CircleCollider2D>().isTrigger = false;
+    }
+
+    public void SetRadius(float radius)
+    {
+        GetComponent<CircleCollider2D>().radius = radius;
+    }
+
+    public void Explode()
+    {
+        GetComponent<CircleCollider2D>().isTrigger = true;
+    }
+}
--- Assets/Scripts/SmallRock.cs
+++ Assets/Scripts/SmallRock.cs
@@ -7,14 +7,21 @@ public class SmallRock : MonoBehaviour
 
     [SerializeField] float health = 1;
     [SerializeField] int scoreValue = 0;
+    [SerializeField] float explosionRadius = 3;
+    [SerializeField] GameObject explosionPrefab;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("BOOM");
-
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
         if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+        CreateExplosion();
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
         if (health <= 0)
         {
             Destroy(gameObject);
@@ -22,4 +29,14 @@ public class SmallRock : MonoBehaviour
         }
     }
 
+    private void CreateExplosion()
+    {
+        GameObject explosion = Instantiate(
+            explosionPrefab,
+            transform.position,
+            Quaternion.identity)
+            as GameObject;
+        explosion.GetComponent<Bomb>().SetRadius(explosionRadius);
+        explosion.GetComponent<Bomb>().Explode();
+    }
 }
