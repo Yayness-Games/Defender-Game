0d6d1e12767a5c34410f512eb89688ea4f26e7dd
TodTheSushiMonster <69654470+TodTheSushiMonster@users.noreply.github.com>
2020-09-11 15:14:11 -0700

    Shop!
    Added a shop system, where you have some coins, and you can use them to buy stuff and it keeps track of what you have

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] int cost;
+    [SerializeField] string itemName;
+
+    public void bought()
+    {
+        Debug.Log("Attempting to buy...");
+        if(GetComponentInParent<Shop>().money >= cost)
+        {
+            Debug.Log("buying...");
+            GetComponentInParent<Shop>().money -= cost;
+            GetComponentInParent<Shop>().addition(itemName);
+        }
+    }
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    public int money = 0;
+    [SerializeField] Text moneyText;
+    [SerializeField] Text inventory;
+
+    public void addition(string item)
+    {
+        moneyText.text = money.ToString();
+        inventory.text += "\n" + item;
+    }
+}
